8db308d84ef8ecc4450cd5e7838a482231bb9d70
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-03 20:52:11 -0500

    Ultima actualizacion

--- app/src/main/java/com/laboratorio/utilizandobd/Informacion.java
+++ app/src/main/java/com/laboratorio/utilizandobd/Informacion.java
@@ -1,4 +1,29 @@
 package com.laboratorio.utilizandobd;
 
-public class Informacion {
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Informacion extends AppCompatActivity {
+
+    private TextView informacion;
+    private Button volver;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        informacion = findViewById(R.id.info);
+        volver = findViewById(R.id.btn_volver);
+
+        databaseHelper=new DatabaseHelper(this);
+        StringBuilder stringBuilder=new StringBuilder();
+        Cursor cursor=databaseHelper.VerDatos();
+    }
+
+    public void Volver() { finish();} // Regresa al MainActivity }
 }
