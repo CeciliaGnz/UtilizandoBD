db47ec3b6c8065efc8b50cc465f65880a3e2c2af
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-05 11:09:02 -0500

    Laboratorio Completo

--- app/src/main/java/com/laboratorio/utilizandobd/Informacion.java
+++ app/src/main/java/com/laboratorio/utilizandobd/Informacion.java
@@ -17,12 +17,31 @@ public class Informacion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.informacion_activity);
+
         informacion = findViewById(R.id.info);
         volver = findViewById(R.id.btn_volver);
 
         databaseHelper=new DatabaseHelper(this);
         StringBuilder stringBuilder=new StringBuilder();
         Cursor cursor=databaseHelper.VerDatos();
+
+        if(cursor.getCount()==0){
+            stringBuilder.append("No hay información registrada.");
+        }else{
+            while(cursor.moveToNext()){
+                String cedula=cursor.getString(0);
+                String nombre=cursor.getString(1);
+                String celular=cursor.getString(2);
+                String contrasena=cursor.getString(3);
+
+                stringBuilder.append("Cédula: ").append(cedula).append("\n");
+                stringBuilder.append("Nombre: ").append(nombre).append("\n");
+                stringBuilder.append("Celular: ").append(celular).append("\n");
+                stringBuilder.append("Contraseña: ").append(contrasena).append("\n");
+            }
+        }
+        informacion.setText(stringBuilder.toString());
     }
 
     public void Volver() { finish();} // Regresa al MainActivity }
